74ca76f9fa343de3ca833c4b6ecbd0e60e3ad736
Dyzaaaa <dyzakhoirun20@gmail.com>
2024-08-11 17:11:35 +0700

    y

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Desa Muncang</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     <link href="https://unpkg.com/flowbite@1.6.4/dist/flowbite.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/flowbite@2.5.1/dist/flowbite.min.css"  rel="stylesheet" />
     @vite(['resources/css/app.css','resources/js/app.js'])
@@ -85,8 +86,6 @@
 </footer>
 
 
-
-    <script src="https://unpkg.com/flowbite@1.6.4/dist/flowbite.js"></script>
 </body>
 
 </html>
