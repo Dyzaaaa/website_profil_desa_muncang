11b38869822966fc689f14d11092b12f38ad00b0
Dyzaaaa <dyzakhoirun20@gmail.com>
2024-08-12 19:41:14 +0700

    y

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,7 +33,7 @@
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
                             </svg>
                         </button>
-                        Dropdown menu
+                        <!-- Dropdown menu -->
                         <div id="dropdownNavbar" class="absolute hidden z-50 font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
                             <ul class="py-2 text-sm text-gray-700 dark:text-gray-400">
                                 <!-- <li>
@@ -48,9 +48,7 @@
                             </ul>
                         </div>
                     </li>
-                    <li>
-                                    <a href="{{ route('struktur') }}" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Struktur Organisasi Desa</a>
-                                </li>
+                    
                     <li>
                         <a href="{{ route('news') }}" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Berita</a>
                     </li>
