6fe54a612e06fbecf9b446097845a1e6fe39aca3
Dyzaaaa <dyzakhoirun20@gmail.com>
2024-08-13 06:41:44 +0700

    y

--- resources/views/admin/layouts/adminapp.blade.php
+++ resources/views/admin/layouts/adminapp.blade.php
@@ -39,11 +39,11 @@
                 </a>
             </li>
             <li>
-                <a href="{{ route('monografi.index') }}" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                <!-- <a href="{{ route('monografi.index') }}" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
   <path fill-rule="evenodd" d="M2.25 13.5a8.25 8.25 0 0 1 8.25-8.25.75.75 0 0 1 .75.75v6.75H18a.75.75 0 0 1 .75.75 8.25 8.25 0 0 1-16.5 0Z" clip-rule="evenodd" />
   <path fill-rule="evenodd" d="M12.75 3a.75.75 0 0 1 .75-.75 8.25 8.25 0 0 1 8.25 8.25.75.75 0 0 1-.75.75h-7.5a.75.75 0 0 1-.75-.75V3Z" clip-rule="evenodd" />
-</svg>
+</svg> -->
 
 
                     <span class="flex-1 ms-3 whitespace-nowrap">Monografi</span>
